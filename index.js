d87070d1e84de9dd3cbbf88043ef5b21e803cfab
Charlie Crisp <charliecrisp249@gmail.com>
2018-06-16 21:17:28 +0100

    Add webpack-dev-middleware back

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const config = require('./webpack.config.dev');
 const app = express();
 const compiler = webpack(config);
 
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.use(express.static(__dirname + '/assets'));
