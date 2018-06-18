0cd6cac7270b1f6f0a95f17b838cda0430aeca9b
Charlie Crisp <charliecrisp249@gmail.com>
2018-06-18 19:17:35 +0100

    Use different intro image with better scaling

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,18 +1,47 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Introduction extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange.bind(this));
+  }
+
+  handleWindowSizeChange() {
+    console.log('triggered');
+    this.setState({ width: window.innerWidth });
+  };
+  
   render() {
-    const { personalInfo } = this.props;
     return (
+      
       <div className="introduction container">
-        <div> 
-          <img className='intro-image' src={'/me.jpg'} />  
-        </div>
+      {this.state.width <= 700 ?
+      <div style={{'flex-direction':'column'}}> 
+        <img src='/me-normal.jpg' className='me-mobile'> </ img>
         <div className='intro-box'>
           <h1>I'm Charlie Crisp!</h1>
           I am a software engineer who graduated from Pembroke College, the University of Cambridge in June 2018.
         </div>
       </div>
+      : 
+      <div className='large-introduction container'> 
+        <div className='intro-text'>
+          <h1>I'm Charlie Crisp!</h1>
+          I am a software engineer who graduated from Pembroke College, the University of Cambridge in June 2018.
+        </div>
+      </div>}
+      </div>
+        
     );
   }
 }
