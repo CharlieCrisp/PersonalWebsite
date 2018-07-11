64c70b2a369af411a82c20396e83a13eaf391a89
Charlie Crisp <charliecrisp249@gmail.com>
2018-07-11 17:29:55 +0100

    Flesh out blog/workshop descriptions

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -2,9 +2,11 @@ const blogsData = [
   {
     title: `Coming Soon - Building with HashGraph Blog`,
     date: 'July 2018',
-    url: '',
+    url: '#',
     image: 'hashgraphbanner.png',
-    description: ``,
+    description: `This is a two part blog about the new HashGraph technology.
+      HashGraph is a technology which could provide similar properties to blockchain but with better properties. 
+      In these blogs I describe what the HashGraph is, how it works and how you can get started building with the HashGraph SDK.`,
     links: []
   },
   {
@@ -12,7 +14,9 @@ const blogsData = [
     date: 'Feb 2018',
     url: 'https://hackersatcambridge.com/workshops/continuous-integration',
     image: 'cibanner.png',
-    description: ``,
+    description: `Continuous Integration is an important part of any software development workflow. 
+      This workshop is aimed at students about to begin a group project and will show you how to set up your own CI workflow.
+      In particular, you will see how to use Gradle and Travis to build and test a simple Java project.`,
     links: {
       'Worksheet':'https://hackersatcambridge.com/workshops/continuous-integration',
       'Recording':'https://www.youtube.com/watch?v=I0E_4L8ILbM'
@@ -23,7 +27,8 @@ const blogsData = [
     date: 'July 2017',
     url: 'https://blogs.msdn.microsoft.com/uk_faculty_connection/2017/07/23/project-prague-what-is-it-and-why-should-you-care/',
     image: 'praguebanner.png',
-    description: ``,
+    description: `Project Prague is a Microsoft project which allows developers to easily build gesture control into their applications.
+      I discovered Project Prague when I was attending the Microsoft Build conference in 2017 and this blog describes what I learnt.`,
     links: []
   },
   {
@@ -31,7 +36,9 @@ const blogsData = [
     date: 'Feb 2017',
     url: 'https://blogs.msdn.microsoft.com/uk_faculty_connection/2017/02/16/a-overview-of-cognitive-data-science-at-hack-cambridge/',
     image: 'recursebanner.png',
-    description: ``,
+    description: `Hack Cambridge is Cambridge's biggest Hackathon, welcoming over 300 students from all over the UK each year. 
+      I was lucky enough to be involved as part of the committee between 2016 and 2018 and this blog is a review of Hack Cambridge 2017.
+      In particular, it describes the popularity of Microsoft Azure at the event.`,
     links: []
   },
   {
@@ -39,7 +46,8 @@ const blogsData = [
     date: 'Apr 2017',
     url: 'https://www.youtube.com/watch?v=2mN8TAQwOVU&t=11s',
     image: 'androidbanner.png',
-    description: ``,
+    description: `Android is a huge platform and learning to develop apps for it is an extremely important skill for any developer. 
+      This workshop was written to introduce people with a familiarity with Java to the Android development process.`,
     links: []
   }
 ];
